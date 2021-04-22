c8bff7b254271aced6fdf9284013498e53fd0dec
tomek <info@tomekness.de>
2021-04-22 09:17:19 +0200

    no message

--- examples/04_onBoardButtonAndLED_Phonk/arduinoCode_04_onBoardButtonAndLED_Phonk/arduinoCode_04_onBoardButtonAndLED_Phonk.ino
+++ examples/04_onBoardButtonAndLED_Phonk/arduinoCode_04_onBoardButtonAndLED_Phonk/arduinoCode_04_onBoardButtonAndLED_Phonk.ino
@@ -97,7 +97,7 @@ void setup()
 
   //Blynk.begin(auth, ssid, pass);
   Blynk.begin(auth, ssid, pass, "tomekness.ddns.net", 8080);
-  
+  pinMode(D3, INPUT);
   // You can also specify server:
   //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
   //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
